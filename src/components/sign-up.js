0138c712bc4e990cce0b301434f2bb11de1e5bb0
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2020-08-18 10:49:03 -0400

    adding sign up

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import fbase from '../config/fire';
 
-class SignIn extends Component {
+class SignUp extends Component {
   constructor(props) {
     super(props);
 
@@ -13,17 +13,17 @@ class SignIn extends Component {
 
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSignInPress = this.handleSignInPress.bind(this);
+    this.handleSignUpPress = this.handleSignUpPress.bind(this);
   }
 
   handleEmailChange(event) { this.setState({ email: event.target.value }); }
 
   handlePasswordChange(event) { this.setState({ password: event.target.value }); }
 
-  handleSignInPress(event) {
+  handleSignUpPress(event) {
     event.preventDefault();
 
-    fbase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+    fbase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
       console.log(u);
     }).catch((err) => {
       console.log(err);
@@ -33,16 +33,16 @@ class SignIn extends Component {
   render() {
     return (
       <div>
-        <h1>Sign In</h1>
+        <h1>Sign Up</h1>
         <form>
           <input type="text" placeholder="email" onChange={this.handleEmailChange} />
           <input type="text" placeholder="password" onChange={this.handlePasswordChange} />
-          <button type="button" onClick={this.handleSignInPress}>Sign In</button>
+          <button type="button" onClick={this.handleSignInPress}>Sign Up</button>
         </form>
-        <p>New to Touch the Fire? Create an account <NavLink to="/signup">here!</NavLink></p>
+        <p>Already have an account? Sign in <NavLink to="/signin">here!</NavLink></p>
       </div>
     );
   }
 }
 
-export default SignIn;
+export default SignUp;
