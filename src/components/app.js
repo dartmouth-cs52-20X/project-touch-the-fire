daa08b7636e6e8b1220ac251f06effef23a55ebd
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2020-08-18 10:46:58 -0400

    adding sign in

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../style.scss';
 import {
   BrowserRouter as Router, Route, NavLink, Switch,
 } from 'react-router-dom';
+import fbase from '../config/fire';
 import Game from '../game';
+import SignIn from './sign-in';
 
 const About = (props) => {
   return <div> All there is to know about me </div>;
@@ -18,37 +20,59 @@ const Nav = (props) => {
       <ul>
         <li><NavLink to="/" exact>Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
         <li><NavLink to="/game">Game</NavLink></li>
+        <li><NavLink to="/signin">Sign In</NavLink></li>
+        <li><NavLink to="/signup">Sign Up</NavLink></li>
       </ul>
     </nav>
   );
-};
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
 };
 
 const FallBack = (props) => {
   return <div>URL Not Found</div>;
 };
 
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route path="/game" component={Game} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route component={FallBack} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.handleAuthChange();
+  }
+
+  handleAuthChange = () => {
+    fbase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <div>Test {this.state.user}</div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route path="/about" component={About} />
+            <Route path="/game" component={Game} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route component={FallBack} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
