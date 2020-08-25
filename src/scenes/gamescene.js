08c390d9e38f54f5e00ca8e8e73e359dad54ab1a
Suraj Srivats <suraj.srivats.22@dartmouth.edu>
2020-08-24 18:01:19 -0700

    pre merg

--- src/scenes/gamescene.js
+++ src/scenes/gamescene.js
@@ -39,7 +39,7 @@ class GameScene extends Scene {
 
   /* Starting template was adapted from phaser intro tutorial at https://phasertutorials.com/creating-a-simple-multiplayer-game-in-phaser-3-with-an-authoritative-server-part-1/ */
   create() {
-    this.socket = io('https://touch-the-fire-api.herokuapp.com/');
+    this.socket = io('http://localhost:9090');
     this.socket.on('connect', () => { console.log('socket.io connected'); });
     this.add.image(this.game.canvas.width, this.game.canvas.height, 'green').setDisplaySize(this.game.canvas.width * MAP_VIEW_MULT, this.game.canvas.height * MAP_VIEW_MULT);
     this.add.image(this.game.canvas.width, this.game.canvas.height + 60, 'fire').setDisplaySize(50, 65);
@@ -90,12 +90,16 @@ class GameScene extends Scene {
       if (this.star) this.star.destroy();
       this.star = this.physics.add.image(starLocation.x, starLocation.y, 'money').setDisplaySize(53, 40);
       this.physics.add.overlap(this.ship, this.star, () => {
+        console.log('pew');
+
+        this.ship.data.base_speed += 0.2;
         this.socket.emit('starCollected');
       });
     });
   }
 
   addOtherPlayers = (playerInfo) => {
+    console.log(playerInfo);
     const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
     if (playerInfo.team === 'blue') {
       otherPlayer.setTint(0x0000ff);
@@ -110,13 +114,13 @@ class GameScene extends Scene {
     this.ship = this.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
     if (playerInfo.team === 'blue') {
       this.ship.setTint(0x0000ff);
+      // this.ship.setMaxVelocity(100);
     } else {
       this.ship.setTint(0x008000);
+      // this.ship.setMaxVelocity(200);
     }
     this.ship.setDrag(100);
     this.ship.setAngularDrag(100);
-    this.ship.setMaxVelocity(200);
-
     // movement translational
     this.cameras.main.startFollow(this.ship);
   }
@@ -125,12 +129,13 @@ class GameScene extends Scene {
     if (this.ship) {
       if (this.cursors.left.isDown || this.cursors.A.isDown) {
         // this.ship.setAngularVelocity(-150);
-        this.ship.setVelocityX(-200);
+        console.log('left');
+        this.ship.setVelocityX(-200 * this.ship.data.base_speed);
         this.ship.setRotation(Math.PI / 2);
         // this.cameras.main.shake();
       } else if (this.cursors.right.isDown || this.cursors.D.isDown) {
         // this.ship.setAngularVelocity(150);
-        this.ship.setVelocityX(200);
+        this.ship.setVelocityX(200 * this.ship.data.base_speed);
         this.ship.setRotation(-Math.PI / 2);
       } else {
         this.ship.setAngularVelocity(0);
@@ -139,10 +144,10 @@ class GameScene extends Scene {
 
       if (this.cursors.up.isDown || this.cursors.W.isDown) {
         // this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-        this.ship.setVelocityY(-200);
+        this.ship.setVelocityY(-200 * this.ship.data.base_speed);
         this.ship.setRotation(Math.PI);
       } else if (this.cursors.down.isDown || this.cursors.S.isDown) {
-        this.ship.setVelocityY(200);
+        this.ship.setVelocityY(200 * this.ship.data.base_speed);
         this.ship.setRotation();
       } else {
         // this.ship.setAcceleration(0);
@@ -153,6 +158,8 @@ class GameScene extends Scene {
       const { x } = this.ship;
       const { y } = this.ship;
       const r = this.ship.rotation;
+      const base = this.ship.base_speed;
+      
       if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
         if ((x > this.game.canvas.width * MAP_VIEW_MULT || x < 0) && (y > this.game.canvas.height * MAP_VIEW_MULT || y < 0)) {
           this.socket.emit('playerMovement', { x: this.ship.oldPosition.x, y: this.ship.oldPosition.y, rotation: this.ship.rotation });
@@ -185,6 +192,7 @@ class GameScene extends Scene {
         x: this.ship.x,
         y: this.ship.y,
         rotation: this.ship.rotation,
+        base_speed: this.ship.base_speed,
       };
     }
   }
